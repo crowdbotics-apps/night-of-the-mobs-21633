4a1cebc3be548cf4dc5ebd3c12e5e2e529424dcb
Crowdbotics <team@crowdbotics.com>
2020-10-17 19:07:27 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/night_of_the_mobs_21633/MainActivity.java
@@ -0,0 +1,15 @@
+package com.night_of_the_mobs_21633;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "night_of_the_mobs_21633";
+    }
+}
---
+++ ios/night_of_the_mobs_21633Tests/night_of_the_mobs_21633Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface night_of_the_mobs_21633 : XCTestCase
+
+@end
+
+@implementation night_of_the_mobs_21633
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
